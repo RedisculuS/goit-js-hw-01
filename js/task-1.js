1ae4f9d6dfed67b671cf02931e35aaf556924866
RedisculuS <Altunina311@gmail.com>
2024-03-14 22:18:24 +0200

    1st commit
    3 tasks are done
    Before mentor`s check

---
+++ js/task-1.js
@@ -0,0 +1,10 @@
+function makeTransaction(quantity, pricePerDroid) {
+  return `You ordered ${quantity} droids worth ${
+    pricePerDroid * quantity
+  } credits!`;
+}
+
+// console.log('Task 1');
+// console.log(makeTransaction(5, 3000));
+// console.log(makeTransaction(3, 1000));
+// console.log(makeTransaction(10, 500));
